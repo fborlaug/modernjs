a2a9b97d3f0c624923f615614d24180dadf5d07c
Frode Borlaug <fbo@ambita.com>
2019-10-01 19:54:28 +0200

    new tvshow

--- tvShow/tvShowRouter.js
+++ tvShow/tvShowRouter.js
@@ -14,5 +14,11 @@ tvShowRouter.get('/', (req, res) => {
    console.log(`Fetching TV Show with id: ${tvShowId}`);
    res.json(tvShowService.getById(tvShowId));
  });
+
+ tvShowRouter.post('/', (req,res) => {
+   console.log(req.body)
+   const newTvShow = tvShowService.createTvShow(req.body.name, req.body.genre);
+   res.json(newTvShow)
+ });
  
  module.exports = tvShowRouter
--- tvShow/tvShowService.js
+++ tvShow/tvShowService.js
@@ -1,8 +1,9 @@
-TvShow = require('./TvShow');
+const TvShow = require('./TvShow');
+const createId = require('../utils/idUtil');
 
 class TvShowService {
    constructor() {
-      this.tvShows = [new TvShow(1, 'Mr.Robot', 'Drama'), new TvShow(2, 'Black Mirror', 'Drama')];
+      this.tvShows = [new TvShow(createId(), 'Mr.Robot', 'Drama'), new TvShow(createId(), 'Black Mirror', 'Drama')];
    }
 
    getAll() {
@@ -10,7 +11,13 @@ class TvShowService {
    }
 
    getById(tvShowId) {
-      return this.tvShows.find(tvShow => tvShow.id = tvShowId);
+      return this.tvShows.find(tvShow => tvShow.id == tvShowId);
+   }
+
+   createTvShow(name, genre) {
+      const newTvShow = new TvShow(createId(), name, genre);
+      this.tvShows.push(newTvShow);
+      return newTvShow;
    }
 }
 
